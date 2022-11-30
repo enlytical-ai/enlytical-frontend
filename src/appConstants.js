5dd43f1dec633e07e31d698ee81c6daef34054ba
Suresh Patel <suresh.patel@nuvoretail.com>
2022-11-30 17:16:01 +0530

    nothing to commit

--- src/appConstants.js
+++ src/appConstants.js
@@ -1,2 +1,2 @@
-//exports.BASE_URL = "https://enlytical.onrender.com/";
-exports.BASE_URL = "http://localhost:5000/";
+exports.BASE_URL = "https://enlytical.onrender.com/";
+//exports.BASE_URL = "http://localhost:5000/";
