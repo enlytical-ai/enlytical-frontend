437cd0cff9d78f3c1876b5159ff9e73a3490b537
Suresh Patel <suresh.patel@nuvoretail.com>
2023-01-10 10:07:43 +0530

    clean code

--- src/components/settings/Title.jsx
+++ src/components/settings/Title.jsx
@@ -1,11 +1,12 @@
-const Title = ({ children }) => {
+const Title = ({ children, style }) => {
     return (
         <div style={{
+
             height: 32,
             display: "flex",
             alignItems: "center",
         }} className="sellerTableHeader" >
-            <h3 style={{ fontSize: "18px", color: "#1565C0" }} >{children}</h3>
+            <h3 style={{ ...style, fontSize: "18px", color: "#1565C0" }} >{children}</h3>
         </div >
     )
 }
